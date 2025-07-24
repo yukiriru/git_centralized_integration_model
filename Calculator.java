bb6a89f8da0a168409305f9692835877d4f7a5bf
yukiriru <yukiriru1222@gmail.com>
2025-07-24 15:09:42 +0900

    fff

--- Calculator.java
+++ Calculator.java
@@ -8,9 +8,6 @@ public class Calculator {
             case '-':
                 result = num1 - num2;
                 break;
-			case '*':
-                result = num1 * num2;
-                break;
             case '/':
                 if (num2 != 0) {
                     result = num1 / num2;
